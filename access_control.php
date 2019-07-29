812ce535c1f2551210233ce9ea94e0e570ce4884
Amid Mirzeyev <50967829+amid32@users.noreply.github.com>
2019-07-29 19:07:56 +0400

    Update access_control.php

--- access_control.php
+++ access_control.php
@@ -10,9 +10,33 @@ $message = ''; #bu kod login girisinde eyer adin veya sifre sef dogru olub olmad
 
 
 #burada fikirlesinki verilenler bazasinda adiniz ve sifreniz var
-$member = ['username' => 'timtest','passw' => '12345','name' => 'My name is PHP'];
+#$member = ['username' => 'timtest','passw' => '12345','name' => 'My name is PHP'];
 #burada fikirlesinki verilenler bazasinda adiniz ve sifreniz var
 
+#bu kod verilenler bazasinda olan adiniz ve sifreniz var sehifenize daxilolanda buradano oxunur
+if (isset($_POST["login"])) {
+	//$id = $_POST["id"];
+	$username = $_POST["username"];
+	$passw = $_POST["passw"];
+	//$name = $_POST["name"];
+
+	$username =  mysqli_real_escape_string($conn, $username);
+	$passw    =  mysqli_real_escape_string($conn, $passw);
+
+   $query = "SELECT * FROM user WHERE username ='{$username}'";
+   $select_user_query = mysqli_query($conn, $query);
+    
+    if (!$select_user_query) {die("QUERY FATLED".mysqli_error($conn));}
+    	while ($row = mysqli_fetch_assoc($select_user_query)) {
+    		$db_id = $row['id'];
+    		$db_username = $row['username'];
+    		$db_passw = $row['passw'];
+    		$db_name = $row['name'];
+    	}
+    	mysqli_close($conn);
+
+}
+#bu kod verilenler bazasinda olan adiniz ve sifreniz var sehifenize daxilolanda buradano oxunur
 
 
 
@@ -41,7 +65,8 @@ if (!$message) {
 	if ($_POST['username'] == $member['username'] && $_POST['passw'] == $member['passw']) {
 
 		#eyer ad ve sifre duzdurse buzaman mesaj giris duzdur
-		$_SESSION['name'] = $member['name']; #buradais butun sehifedi istifade ede bileceyi verilenler qeydedebilecem
+		#$_SESSION['name'] = $member['name']; #buradais butun sehifedi istifade ede bileceyi verilenler qeydedebilecem
+		$_SESSION['name'] = $db_name;
 		header('location:index.php');
 		exit();
 		
